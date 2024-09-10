af91ca6db2802d02465c3344ba3094cc2f3b847a
Seokxkyu <kyuseok000@gmail.com>
2024-09-10 10:27:10 +0900

    modules

--- src/fishregression/knn.py
+++ src/fishregression/knn.py
@@ -2,7 +2,7 @@ import pickle
 from sklearn.neighbors import KNeighborsClassifier
 
 def knn_api(length, weight):
-    with open(file='/home/kyuseok00/code/fishregression/src/fishregression/model.pkl', mode='rb') as f:
+    with open(file='/home/kyuseok00/code/fishregression/note/knn_model.pkl', mode='rb') as f:
         fish = pickle.load(f)
     
     pred = fish.predict([[length, weight]])
--- src/fishregression/lr.py
+++ src/fishregression/lr.py
@@ -3,10 +3,14 @@ from fishregression.mmanager import get_model_path
 from sklearn.linear_model import LinearRegression
 
 def lr_api(length):
-    path = get_model_path()
+    # path = get_model_path()
 
-    with open(file=path, mode='rb') as f:
-        lr = pickle.load(f)
+    # with open(file=path, mode='rb') as f:
+    #    lr = pickle.load(f)
     
+    with open(file='/home/kyuseok00/code/fishregression/note/lr_model.pkl', mode='rb') as f:
+        lr = pickle.load(f)
+
     prediction = lr.predict([[length]])
     return prediction[0]
+
