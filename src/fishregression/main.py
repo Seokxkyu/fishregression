a906e35163f197b555e5989304f644d5a05db4b5
Seokxkyu <kyuseok000@gmail.com>
2024-09-10 11:40:22 +0900

    main.py fix

--- src/fishregression/main.py
+++ src/fishregression/main.py
@@ -1,24 +1,38 @@
 from typing import Union
+from fishregression.api.lr import lr_api
+from fishregression.api.knn import knn_api
 from fastapi import FastAPI
-import pickle
-from fr.mmanager import get_model_path
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "fr"}
+    return {"Hello": "Fish Regressor"}
+
+@app.get("/lr")
+def read_item(l: float):
+    """
+    물고기의 무게 예측
+
+    Args:
+        length (float): 물고기 길이(cm)
+
+    Returns:
+        dict:
+          weight (float): 물고기 무게(g)
+    """
+    length_poly = np.array([l**2, l]).reshape(1, -1)
+    weight = lr_api(length_poly)
+    return {"weight": weight}
+
+@app.get("/knn")
+def read_item(w: float, l: float):
+    fish_class = knn_api(l, w)
+    result_msg = f"🐟 길이 {l}에 무게 {w}인 물고기는 {fish_class}로 예측됩니다!"
+    return {"result": result_msg}
 
 
-def run_prediction(length: float):
-    model_path = get_model_path()
-    with open(model_path, "rb") as f:
-        model = pickle.load(f)
-    r = model.predict([[length**2, length]])
-    return float(r[0])
-
-    
-
+'''
 @app.get("/fish")
 def fish(length: float):
     """
@@ -38,3 +52,4 @@ def fish(length: float):
                 "length": length, 
                 "weight": fish_weight
             }
+'''
