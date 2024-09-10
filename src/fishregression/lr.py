59e1a010d893f3129869559b705de16d6b6e3039
Seokxkyu <kyuseok000@gmail.com>
2024-09-10 09:44:24 +0900

    model init

--- src/fishregression/lr.py
+++ src/fishregression/lr.py
@@ -4,7 +4,9 @@ from sklearn.linear_model import LinearRegression
 
 def lr_api(length):
     path = get_model_path()
+
     with open(file=path, mode='rb') as f:
         lr = pickle.load(f)
+    
     prediction = lr.predict([[length]])
     return prediction[0]
